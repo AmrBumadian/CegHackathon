41881d101a5756c04a196a0bbf07febad1926668
Ahmad Waleed <a.waleedothman@gmail.com>
2022-03-12 15:23:55 +0200

    backend initial commit

---
+++ server/vaccinationPointManager/src/main/java/com/team5/vaccinationpointmanager/VaccinationPointManagerApplication.java
@@ -0,0 +1,13 @@
+package com.team5.vaccinationpointmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationPointManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationPointManagerApplication.class, args);
+    }
+
+}
